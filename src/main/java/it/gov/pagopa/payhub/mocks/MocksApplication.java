0b41c9aabb25272fb3e3cb4d0e0850cea87d8e64
antoniocalo <antonio.calo@nttdata.com>
2024-11-27 11:58:41 +0100

    P4ADEV-1447 initialize Repository

--- src/main/java/it/gov/pagopa/payhub/mocks/MocksApplication.java
+++ src/main/java/it/gov/pagopa/payhub/mocks/MocksApplication.java
@@ -1,13 +1,13 @@
-package it.gov.pagopa.template;
+package it.gov.pagopa.payhub.mocks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TemplateApplication {
+public class MocksApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TemplateApplication.class, args);
+		SpringApplication.run(MocksApplication.class, args);
 	}
 
 }
