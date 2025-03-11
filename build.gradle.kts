33792d0c254ddc41cc09cbdc818e06f4160c5caf
antonio.torre <antonio.torre@pagopa.it>
2025-03-11 18:50:47 +0100

    feat: Configuring build-info on actuator

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.3.5"
-	id("io.spring.dependency-management") version "1.1.6"
-	jacoco
-	id("org.sonarqube") version "5.1.0.4882"
-	id("com.github.ben-manes.versions") version "0.51.0"
-	id("org.openapi.generator") version "7.9.0"
+  java
+  id("org.springframework.boot") version "3.3.5"
+  id("io.spring.dependency-management") version "1.1.6"
+  jacoco
+  id("org.sonarqube") version "5.1.0.4882"
+  id("com.github.ben-manes.versions") version "0.51.0"
+  id("org.openapi.generator") version "7.9.0"
+  id("com.gorylenko.gradle-git-properties") version "2.5.0"
 }
 
 group = "it.gov.pagopa.payhub"
@@ -13,19 +14,19 @@ version = "0.0.1"
 description = "p4pa-mocks"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(21)
+  }
 }
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+  compileOnly {
+    extendsFrom(configurations.annotationProcessor.get())
+  }
 }
 
 repositories {
-	mavenCentral()
+  mavenCentral()
 }
 
 val springDocOpenApiVersion = "2.6.0"
@@ -40,77 +41,78 @@ val wiremockSpringBootVersion = "2.1.3"
 val javaFakerVersion = "1.0.2"
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springDocOpenApiVersion")
-	implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
-	implementation("org.openapitools:jackson-databind-nullable:$openApiToolsVersion")
-	implementation("com.google.code.findbugs:jsr305:$findbugsVersion")
+  implementation("org.springframework.boot:spring-boot-starter")
+  implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("org.springframework.boot:spring-boot-starter-actuator")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springDocOpenApiVersion")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+  implementation("org.openapitools:jackson-databind-nullable:$openApiToolsVersion")
+  implementation("com.google.code.findbugs:jsr305:$findbugsVersion")
   implementation("com.github.javafaker:javafaker:$javaFakerVersion") {
     exclude(group = "org.yaml", module = "snakeyaml")
   }
 
   compileOnly("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok")
+  annotationProcessor("org.projectlombok:lombok")
 
-	// validation token jwt
-	implementation("com.auth0:java-jwt:$javaJwtVersion")
-	implementation("com.auth0:jwks-rsa:$jwksRsaVersion")
-	implementation("com.nimbusds:nimbus-jose-jwt:$nimbusJoseJwtVersion")
-	implementation("io.jsonwebtoken:jjwt-api:$jjwtVersion")
+  // validation token jwt
+  implementation("com.auth0:java-jwt:$javaJwtVersion")
+  implementation("com.auth0:jwks-rsa:$jwksRsaVersion")
+  implementation("com.nimbusds:nimbus-jose-jwt:$nimbusJoseJwtVersion")
+  implementation("io.jsonwebtoken:jjwt-api:$jjwtVersion")
 
-	//	Testing
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.mockito:mockito-core")
-	testImplementation ("org.projectlombok:lombok")
-	testImplementation ("org.wiremock:wiremock-standalone:$wiremockVersion")
-	testImplementation ("com.maciejwalkowiak.spring:wiremock-spring-boot:$wiremockSpringBootVersion")
+  //	Testing
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation("org.mockito:mockito-core")
+  testImplementation("org.projectlombok:lombok")
+  testImplementation("org.wiremock:wiremock-standalone:$wiremockVersion")
+  testImplementation("com.maciejwalkowiak.spring:wiremock-spring-boot:$wiremockSpringBootVersion")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
-	finalizedBy(tasks.jacocoTestReport)
+  useJUnitPlatform()
+  finalizedBy(tasks.jacocoTestReport)
 }
 
 tasks.jacocoTestReport {
-	dependsOn(tasks.test)
-	reports {
-		xml.required = true
-	}
+  dependsOn(tasks.test)
+  reports {
+    xml.required = true
+  }
 }
 
 val projectInfo = mapOf(
-	"artifactId" to project.name,
-	"version" to project.version
+  "artifactId" to project.name,
+  "version" to project.version
 )
 
 tasks {
-	val processResources by getting(ProcessResources::class) {
-		filesMatching("**/application.yml") {
-			expand(projectInfo)
-		}
-	}
+  val processResources by getting(ProcessResources::class) {
+    filesMatching("**/application.yml") {
+      expand(projectInfo)
+    }
+  }
 }
 
 configurations {
-	compileClasspath {
-		resolutionStrategy.activateDependencyLocking()
-	}
+  compileClasspath {
+    resolutionStrategy.activateDependencyLocking()
+  }
 }
 
 tasks.compileJava {
-	dependsOn("openApiGenerateAnprApiC030", "openApiGenerateAnprApiC003")
+  dependsOn("openApiGenerateAnprApiC030", "openApiGenerateAnprApiC003")
 }
 
 configure<SourceSetContainer> {
-	named("main") {
-		java.srcDir("$projectDir/build/generated/src/main/java")
-	}
+  named("main") {
+    java.srcDir("$projectDir/build/generated/src/main/java")
+  }
 }
 
 springBoot {
-	mainClass.value("it.gov.pagopa.payhub.mocks.MocksApplication")
+  buildInfo()
+  mainClass.value("it.gov.pagopa.payhub.mocks.MocksApplication")
 }
 
 tasks.register<org.openapitools.generator.gradle.plugin.tasks.GenerateTask>("openApiGenerateAnprApiC030") {
