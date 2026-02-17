b48935c91fb1f7332f5e4788eed50f5712c90491
MScarsella <marco.scarsella@pagopa.it>
2026-02-17 17:43:32 +0100

    fix: P4ADEV-4470 fix cieOnline baseUrl (#24)

--- src/main/java/it/gov/pagopa/payhub/mocks/cie/controller/CiePaymentsController.java
+++ src/main/java/it/gov/pagopa/payhub/mocks/cie/controller/CiePaymentsController.java
@@ -5,10 +5,12 @@ import it.gov.pagopa.payhub.cie.controller.generated.PaymentsApi;
 import it.gov.pagopa.payhub.cie.model.generated.CiePaymentResponse;
 import it.gov.pagopa.payhub.mocks.cie.service.CiePaymentsService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Cie")
 @RestController
+@RequestMapping("/cie")
 public class CiePaymentsController implements PaymentsApi {
   private final CiePaymentsService ciePaymentsService;
 
