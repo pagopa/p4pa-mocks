337178cdd33bf45ef8c9affd25cabff45f12744b
MScarsella <marco.scarsella@pagopa.it>
2026-02-26 16:07:30 +0100

    fix: Revert "feat: Promote to UAT" (#27)

--- src/main/java/it/gov/pagopa/payhub/mocks/cie/service/CiePaymentsService.java
+++
@@ -1,7 +0,0 @@
-package it.gov.pagopa.payhub.mocks.cie.service;
-
-import it.gov.pagopa.payhub.cie.model.generated.CiePaymentResponse;
-
-public interface CiePaymentsService {
-  CiePaymentResponse getFees(String issuerFiscalCode, String releaseCodeReason);
-}
