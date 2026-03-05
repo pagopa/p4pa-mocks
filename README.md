51b6d84f414b6a9e16fffe89d7bbb4c3bff2ac6a
michele.mazzini <michele.mazzini1996@gmail.com>
2026-03-05 12:03:37 +0100

    fix: apply requested changes

--- README.md
+++ README.md
@@ -29,6 +29,21 @@ This repository contains the mock implementations of the services required for t
       * `SILNODT`: it will return error 003 (notification date not available)
     * Pseudo-randomly, it will return also a (single) balance
 
+### CIE
+* [cie-online.openapi.json](openapi/cie/cie-online.openapi.json)
+  * `getIssuerFC`
+    * Returns a list of available issuers, each including its name, province code, and Fiscal Code.
+    * The list contains a set of **static pre-defined issuers**:
+      * Ente P4PA intermediato 2 (BG)
+      * Comune di Brescia (BS)
+      * Comune di Milano (MI)
+    * It **dynamically includes** the "Comune di Test" (TS - `11111111111`) based on the configuration set via the `includeCieTestOrganization` API.
+
+### Mock Configuration APIs
+* [mock-configuration.openapi.yaml](openapi/mock-configuration/mock-configuration.openapi.yaml)
+  * `includeCieTestOrganization`
+    * This API allows for dynamic configuration of the CIE mock behavior at runtime.
+   
 ## 📂 Repository Structure
 
 Here is a quick overview of the files and directories included in this repository:
--- src/main/java/it/gov/pagopa/payhub/mocks/controller/CieMockConfigurationController.java
+++ src/main/java/it/gov/pagopa/payhub/mocks/controller/CieMockConfigurationController.java
@@ -1,20 +1,20 @@
 package it.gov.pagopa.payhub.mocks.controller;
 
-import it.gov.pagopa.payhub.mockconfiguration.controller.generated.MockConfigurationApi;
+import it.gov.pagopa.payhub.mockconfiguration.controller.generated.CieMockConfigurationApi;
 import it.gov.pagopa.payhub.mocks.cie.service.CiePaymentsService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MockConfigurationController implements MockConfigurationApi {
+public class CieMockConfigurationController implements CieMockConfigurationApi {
   private final CiePaymentsService ciePaymentsService;
 
-  public MockConfigurationController(CiePaymentsService ciePaymentsService) {
+  public CieMockConfigurationController(CiePaymentsService ciePaymentsService) {
     this.ciePaymentsService = ciePaymentsService;
   }
 
   @Override
-  public ResponseEntity<Void> setIncludeTestIssuer(Boolean include) {
+  public ResponseEntity<Void> includeCieTestOrganization(Boolean include) {
     ciePaymentsService.setIncludeTestIssuer(include);
     return ResponseEntity.ok().build();
   }
