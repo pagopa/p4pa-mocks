56f5b2bf8c7222b225f324c3575ae8a69ba6e5c8
antonio.torre <antonio.torre@pagopa.it>
2026-07-21 17:15:11 +0200

    feat: Migrating p4pa-mocks on Argo

--- .github/terraform/99_locals.tf
+++ .github/terraform/99_locals.tf
@@ -4,29 +4,25 @@ locals {
     CreatedBy   = "Terraform"
     Environment = var.env
     Owner       = upper(var.prefix)
-    Source      = "" # Repository URL
-    CostCenter  = ""
+    Source      = "https://github.com/pagopa/p4pa-mocks" # Repository URL
+    CostCenter  = "TS310 - PAGAMENTI & SERVIZI"
   }
 
   # Repo
   github = {
     org        = "pagopa"
-    repository = "" # Repository Name
+    repository = "p4pa-mocks" # Repository Name
   }
 
-  env_secrets = {
-    ENV_SECRET = "data.azurerm_key_vault_secret.CHANGE_ME.value"
-  }
-  env_variables = {
-    ENV_VARIABLE = "ENV_VARIABLE"
-  }
+  env_secrets   = {}
+  env_variables = {}
 
   repo_secrets = var.env_short == "p" ? {
-    SECRET = "SECRET"
+    ADMIN_GITHUB_TOKEN_RW = data.azurerm_key_vault_secret.github_token[0].value
+    AZURE_DEVOPS_TOKEN    = data.azurerm_key_vault_secret.azure_devops_token[0].value
   } : {}
-  repo_env = var.env_short == "p" ? {
-    ENV_VARIABLE = "ENV_VARIABLE"
-  } : {}
+
+  repo_env = {}
 
   map_repo = {
     "dev" : "*",
